cf43f58088c10cb5e0212f41598e2313185c4747
Dimitrios-Eleftherios Diannelos <jdian18@gmail.com>
2025-05-03 01:22:04 +0300

    remove zig language lsp

--- lua/core/lazy/lsp.lua
+++ lua/core/lazy/lsp.lua
@@ -36,22 +36,6 @@ return {
                 function(server_name)
                     require('lspconfig')[server_name].setup { capabilities = capabilities }
                 end,
-
-                zls = function()
-                    local lspconfig = require('lspconfig')
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
                 ["lua_ls"] = function()
                     local lspconfig = require('lspconfig')
                     lspconfig.lua_ls.setup {
